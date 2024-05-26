c3d939e8227f37d9974558547058342d17a45b9e
Vincent <vjc9@duke.edu>
2024-05-25 22:33:24 -0500

    fixed progress bar

--- src/containers/skillProgress/skillProgress.js
+++ src/containers/skillProgress/skillProgress.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useContext} from "react";
 import "./Progress.scss";
 import {illustration, techStack} from "../../portfolio";
 import {Fade} from "react-reveal";
 import Build from "../../assets/lottie/build";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import StyleContext from "../../contexts/StyleContext";
 
 export default function StackProgress() {
+  const {isDark} = useContext(StyleContext);
   if (techStack.viewSkillBars) {
     return (
       <Fade bottom duration={1000} distance="20px">
@@ -14,7 +16,18 @@ export default function StackProgress() {
             <h1 className="skills-heading">Proficiency</h1>
             {techStack.experience.map((exp, i) => {
               const progressStyle = {
-                width: exp.progressPercentage
+                width: exp.progressPercentage,
+                backgroundColor: (() => {
+                  if (parseInt(exp.progressPercentage, 10) < 25) {
+                    return "#fe797b";
+                  } else if (parseInt(exp.progressPercentage, 10) < 70) {
+                    return "#ffb750";
+                  } else if (parseInt(exp.progressPercentage, 10) < 85) {
+                    return "#8fe968";
+                  } else {
+                    return "#36CEDC";
+                  }
+                })()
               };
               return (
                 <div key={i} className="skill">
@@ -25,6 +38,33 @@ export default function StackProgress() {
                 </div>
               );
             })}
+            <p>&nbsp;</p>
+            <div className={isDark ? "info-box-dark" : "info-box-light"}>
+              <pre>
+                <i class="fa-solid fa-circle-info"></i>
+                {"  "}
+                <i
+                  class="fa-solid fa-circle small-space"
+                  style={{color: "#36CEDC"}}
+                ></i>
+                Expert{"  "}
+                <i
+                  class="fa-solid fa-circle small-space"
+                  style={{color: "#8fe968"}}
+                ></i>
+                Advanced{"  "}
+                <i
+                  class="fa-solid fa-circle small-space"
+                  style={{color: "#ffb750"}}
+                ></i>
+                Intermediate{"  "}
+                <i
+                  class="fa-solid fa-circle small-space"
+                  style={{color: "#fe797b"}}
+                ></i>
+                Beginner
+              </pre>
+            </div>
           </div>
 
           <div className="skills-image">
